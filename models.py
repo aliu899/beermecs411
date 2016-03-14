404d7d182955f0b7fec9585ca84873f0424cefbf
aliu899 <a.liu899@gmail.com>
2016-03-13 23:27:03 -0500

    SAD

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 def create_user(email_address, password):
     print "start"
-    db.session.execute("INSERT INTO User (email, password) VALUES (:email_address, :password)")
+    db.session.execute("INSERT INTO User (email, password) VALUES (:email, :password)", {"email": email_address, "password": password})
     print "end"
 
 def list_users():
