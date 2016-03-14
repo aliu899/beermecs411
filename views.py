580334349c6df919a2146c47d4d2c7ae1168af8b
ltmarsh2 <laurenmarshall@wirelessprvnat-172-17-232-117.near.illinois.edu>
2016-03-13 19:53:13 -0500

    view changes

--- views.py
+++ views.py
@@ -1,9 +1,8 @@
 __author__ = 'Aaron'
 
 from app import app
-from flask import render_template, request
-from models import *
+from flask import render_template
 
 @app.route("/")
 def hello():
-    return "Hello World!!!!!!!!"
+    return render_template('index.html')
