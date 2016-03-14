58c628c12a813470e5d04155f4105f2c260d4809
aliu899 <a.liu899@gmail.com>
2016-03-13 22:19:48 -0500

    signup form

--- models.py
+++ models.py
@@ -1,2 +1,7 @@
 __author__ = 'Aaron'
 from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+
+def create_user(email_address, password):
+    db.engine.execute("INSERT INTO User (email, password) VALUES (?, ?)", (email_address, password))
--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 __author__ = 'Aaron'
 
 from app import app
-from flask import render_template
+from models import *
+from flask import render_template, request
 
 @app.route("/")
 def home_page():
@@ -9,5 +10,10 @@ def home_page():
 
 @app.route("/signup.html")
 def sign_up():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if '@' in email and password == request.form['password_confirm']:
+            create_user(email, password)
     return render_template('signup.html')
 
