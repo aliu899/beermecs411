eba19c263613aacbf283da10a218d82ef22f0af6
aliu899 <a.liu899@gmail.com>
2016-03-13 19:48:21 -0500

    setup postgres

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 __author__ = 'Aaron'
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
