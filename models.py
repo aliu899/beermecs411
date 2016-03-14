a0c7481f2c315e31294bec3a12abff8dfb28b264
AndrewMogensen <mogense2@illinois.edu>
2016-03-13 23:12:32 -0500

    insert10

--- models.py
+++ models.py
@@ -4,8 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
 def create_user(email_address, password):
-	sql = "INSERT INTO User (email, password) VALUES (abc@123.com, password)"
-    db.engine.execute(sql)
+    db.engine.execute("INSERT INTO User (email, password) VALUES (abc@123.com, password)")
 
 def list_users():
     result = db.engine.execute("SELECT * FROM User")
