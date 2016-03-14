c9ef6d9852b26ccf76228b52d4bb9d15a56be461
ltmarsh2 <laurenmarshall@wirelessprvnat-172-17-232-117.near.illinois.edu>
2016-03-13 23:08:45 -0500

    UI tweaks. Settings page added.

--- models.py
+++ models.py
@@ -1,2 +1,11 @@
 __author__ = 'Aaron'
 from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+
+def create_user(email_address, password):
+    db.engine.execute("INSERT INTO User (email, password) VALUES (?, ?)", (email_address, password))
+
+def list_users():
+    result = db.engine.execute("SELECT * FOM User")
+    return result
--- views.py
+++ views.py
@@ -1,15 +1,24 @@
 __author__ = 'Aaron'
 
 from app import app
-from flask import render_template
+from models import *
+from flask import render_template, request
 
 @app.route("/")
 def home_page():
     return render_template('index.html')
 
-@app.route("/signup.html")
+@app.route("/signup.html", methods=["GET","POST"])
 def sign_up():
-    return render_template('signup.html')
+    result = []
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if '@' in email and password == request.form['password_confirm']:
+            print "hello"
+            create_user(email, password)
+            result = list_users()
+    return render_template('signup.html', users = result)
 
 @app.route("/settings.html")
 def settings_page():
