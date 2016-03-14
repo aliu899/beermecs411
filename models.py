a0ff203d0b063eb1a8bddba5e98ce6d494c6bd89
AndrewMogensen <mogense2@illinois.edu>
2016-03-13 23:04:19 -0500

    insert test5

--- models.py
+++ models.py
@@ -1,11 +1,9 @@
 __author__ = 'Aaron'
 from app import db
 from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy import text
 
 def create_user(email_address, password):
 	txt = "INSERT INTO User (email, password) VALUES (\'" + email + "\', \'"  + password + "\')"
-	#sql = text(txt)
     db.engine.execute(txt)
 
 def list_users():
