0846381e44abaf97ec7376cf60f822104c55ead5
AndrewMogensen <mogense2@illinois.edu>
2016-03-14 11:28:43 -0500

    insert1236

--- models.py
+++ models.py
@@ -5,7 +5,8 @@ from sqlalchemy import *
 
 def create_user(email_address, password):
 	print "insert"
-	db.engine.execute("INSERT INTO \"User\" (email, password) VALUES (\'?\', \'?\');", email_address, password)
+	execution_str = "INSERT INTO \"User\" (email, password) VALUES (\'" + email_address + "\', \'" + password + "\');"
+	db.engine.execute(execution_str)
 
 def list_users():
     result = db.engine.execute("SELECT * FROM \"User\";")
