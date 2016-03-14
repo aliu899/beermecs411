d1dabfaff3c8dba85d9a7d8baba09ce8596334af
aliu899 <a.liu899@gmail.com>
2016-03-13 23:29:11 -0500

    WE

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 __author__ = 'Aaron'
 from app import db
+from sqlalchemy import text
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 
 def create_user(email_address, password):
     print "start"
-    db.session.execute("INSERT INTO User (email, password) VALUES (:email, :password)", {"email": email_address, "password": password})
+    db.session.execute(text("INSERT INTO User (email, password) VALUES (:email, :password)"), {"email": email_address, "password": password})
     print "end"
 
 def list_users():
