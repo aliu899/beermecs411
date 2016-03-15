d2db249fcb4475e4ed1e7565384bb6f1a4830fcc
aliu899 <a.liu899@gmail.com>
2016-03-14 19:10:14 -0500

    SAD

--- models.py
+++ models.py
@@ -14,8 +14,6 @@ def list_users():
 def verify_user(email_address, password):
     execution_str = "SELECT * FROM \"User\" WHERE email=\'" + email_address +"\' AND password=\'" + password + "\';"
     result = db.engine.execute(execution_str)
-    if len(result) != 0:
+    for item in result:
         return true
-    else:
-        print "rejected"
-        return false
+    return false
