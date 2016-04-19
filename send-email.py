90a3aa4ebd28d725e1af1e423a7c7711c5e88b53
ltmarsh2 <laurenmarshall@wirelessprvnat-172-17-170-86.near.illinois.edu>
2016-04-18 20:39:29 -0500

    Email Python code

---
+++ send-email.py
@@ -0,0 +1,13 @@
+import smtplib
+
+content = """From: Beer Me <beer.me.application@gmail.com>
+Subject: Beer Me Update
+
+We found some changes in beer prices that might interest you. """
+mail = smtplib.SMTP('smtp.gmail.com', 587)
+mail.ehlo()
+
+mail.starttls()
+mail.login('beer.me.application@gmail.com','beerme411')
+mail.sendmail('beer.me.application@gmail.com','lauren.taylor.marshall@gmail.com', content)
+mail.close()
