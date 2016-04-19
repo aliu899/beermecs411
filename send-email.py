0a2c64f565928f2704cf0d4ab27cc23161be40ed
AndrewMogensen <mogense2@illinois.edu>
2016-04-19 14:13:43 -0500

    emailing

--- send-email.py
+++ send-email.py
@@ -1,13 +1,8 @@
 #import smtplib
 from models import get_notify_info
 
-notifications = get_notify_info()
-for n in notifications:
-	email(n)
-
 def email(notification):
-	print notification
-
+    print notification
 
 #	content = """From: Beer Me <beer.me.application@gmail.com>
 #	Subject: Beer Me Price Update
@@ -20,3 +15,8 @@ def email(notification):
 #	mail.login('beer.me.application@gmail.com','beerme411')
 #	mail.sendmail('beer.me.application@gmail.com','lauren.taylor.marshall@gmail.com', content)
 #	mail.close()
+
+notifications = get_notify_info()
+for n in notifications:
+    email(n)
+
