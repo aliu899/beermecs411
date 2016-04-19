bbd10993a32ea192aff0e4a171f07c89a55b54f6
AndrewMogensen <mogense2@illinois.edu>
2016-04-19 14:25:18 -0500

    emailing

--- send-email.py
+++ send-email.py
@@ -2,7 +2,8 @@
 from models import get_notify_info
 
 def email(notification):
-    print notification
+    body = "We found a good deal on one of your favorite beers that might interest you.  " + notification[4] + " is selling a " + notification[2] + "oz " + notification[3] + "pk of " + notification[0] + " for $" + notification[1]
+    print body
 
 #	content = """From: Beer Me <beer.me.application@gmail.com>
 #	Subject: Beer Me Price Update
