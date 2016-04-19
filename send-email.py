648fb4a69291c2c66acc5d4b12ce9015242b68c9
AndrewMogensen <mogense2@illinois.edu>
2016-04-19 16:24:13 -0500

    emailing

--- send-email.py
+++ send-email.py
@@ -1,5 +1,5 @@
 #import smtplib
-import models
+from models import get_notify_info, update_best_value
 
 def email(notification):
     body = "We found a good deal on one of your favorite beers that might interest you.  " + str(notification[4]) + " is selling a " + str(notification[2]) + "oz " + str(notification[3]) + "pk of " + str(notification[0]) + " for $" + str(notification[1]) + "!"
