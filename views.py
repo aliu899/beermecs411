93eb30e1d59bbd78f7124977246c51024e13684a
AndrewMogensen <mogense2@illinois.edu>
2016-04-19 17:31:59 -0500

    beeradv ratings

--- views.py
+++ views.py
@@ -30,7 +30,7 @@ def home_page():
                         print count
                         break
                     if key.replace("'","") in beer[0].lower():
-                        print beer[0]
+                        print beer[0], beer_dir[key]['rAvg'], beer_dir[key]['style'].replace("'",""), beer_dir[key]['brewery'].replace("'","")
                         add_beer_info(beer[0], beer_dir[key]['rAvg'], beer_dir[key]['styl    e'].replace("'",""), beer_dir[key]['brewery'].replace("'",""))
                         print "beer3"
     except TypeError as e:
