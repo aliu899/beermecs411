dc81e10c994ff83888ffaa2dd6b18413898998f9
AndrewMogensen <mogense2@illinois.edu>
2016-04-19 20:00:56 -0500

    emailing me

--- send-email.py
+++ send-email.py
@@ -5,7 +5,7 @@ from models import get_notify_info, update_best_value
 def email(notification):
     msg = MIMEText("We found a good deal on one of your favorite beers that might interest you.  " + str(notification[4]) + " is selling a " + str(notification[2]) + "oz " + str(notification[3]) + "pk of " + str(notification[0]) + " for $" + str(notification[1]) + "!")
     update_best_value(notification[5], notification[0], notification[6])
-    msg['Subject'] = Beer Me Price Update
+    msg['Subject'] = "Beer Me Price Update"
     msg['To'] = notification[5]
     msg['From'] = 'beer.me.application@gmail.com'
 
