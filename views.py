605e7377dc430e68c0b203bc42589c254a863ce1
aliu899 <a.liu899@gmail.com>
2016-04-20 20:42:20 -0500

    fixed sort

--- views.py
+++ views.py
@@ -73,10 +73,9 @@ def user_dashboard():
         for beer in beers:
             beers_predicted.append((beer[0], float(beer[1]), beer[2]))
         favorite_style = 'Recommended for you...'
+        sorted(beers_predicted, key=lambda rating: rating[1], reverse = True)
     for beer in beers_predicted:
-        print beer[0], beer[1]
-    sorted(beers_predicted, key=lambda rating: rating[1], reverse = True)
-
+            print beer[0], beer[1]
     top3 = [beers_predicted[0], beers_predicted[1], beers_predicted[2]]
     return render_template('query.html', results = search_hits, based_on = favorite_style, recommended = top3)
 
